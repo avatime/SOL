05bbf73832a7d4abec6617ba575d6f414bba996b
이지나 <dlwlsk0428@naver.com>
2022-10-19 08:49:33 +0900

    feat: 회원가입

--- backend/src/main/kotlin/com/finance/backend/auth/AuthController.kt
+++ backend/src/main/kotlin/com/finance/backend/auth/AuthController.kt
@@ -6,7 +6,9 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @RequestMapping("/api/v1/auth")
 class AuthController (private val userService: UserService) {
 
--- backend/src/main/kotlin/com/finance/backend/user/UserServiceImpl.kt
+++ backend/src/main/kotlin/com/finance/backend/user/UserServiceImpl.kt
@@ -1,14 +1,18 @@
 package com.finance.backend.user
 
 import com.finance.backend.common.util.JwtUtils
+import lombok.RequiredArgsConstructor
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
 
+@Service("UserService")
+@RequiredArgsConstructor
 class UserServiceImpl (
         private val userRepository: UserRepository,
         private val passwordEncoder: BCryptPasswordEncoder,
-        private val authenticationManager: AuthenticationManager,
+//        private val authenticationManager: AuthenticationManager,
         private val jwtUtils: JwtUtils
         ) : UserService {
     override fun saveUser(userDto: UserDto) {
