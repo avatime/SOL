a4ba970fdacb98473d553e695049d9cde6ca3ab7
이주영 <dlendjs93@gmail.com>
2022-10-20 15:39:02 +0900

    feat: Splash 화면 추가 및 로그인 여부에 따라 분기 처리 추가

--- android/app/src/main/java/com/finance/android/FinanceApp.kt
+++ android/app/src/main/java/com/finance/android/FinanceApp.kt
@@ -1,12 +1,32 @@
 package com.finance.android
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.finance.android.ui.fragments.LoginFragment
 import com.finance.android.ui.fragments.MainFragment
+import com.finance.android.ui.fragments.SplashFragment
 import com.finance.android.ui.theme.FinanceTheme
+import com.finance.android.utils.Const
 
 @Composable
 fun FinanceApp() {
     FinanceTheme {
-        MainFragment()
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = Const.Routes.SPLASH
+        ) {
+            composable(Const.Routes.SPLASH) {
+                SplashFragment(navController = navController)
+            }
+            composable(Const.Routes.LOGIN) {
+                LoginFragment()
+            }
+            composable(Const.Routes.MAIN) {
+                MainFragment()
+            }
+        }
     }
 }
