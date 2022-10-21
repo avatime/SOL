53cce16f0aaae7451aca1607df484da70e8a0433
이지나 <dlwlsk0428@naver.com>
2022-10-21 10:19:14 +0900

    feat: login

--- backend/src/main/kotlin/com/finance/backend/common/util/JwtFilter.kt
+++ backend/src/main/kotlin/com/finance/backend/common/util/JwtFilter.kt
@@ -12,17 +12,20 @@ import javax.servlet.http.HttpServletResponse
 class JwtFilter(private val jwtUtils: JwtUtils) : OncePerRequestFilter() {
 
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+        println("sjafpodpjapfsf")
         // 헤더에 Authorization이 있다면 가져온다.
         val authorizationHeader: String? = request.getHeader("Authorization") ?: return filterChain.doFilter(request, response)
         // Bearer타입 토큰이 있을 때 가져온다.
         val token = authorizationHeader?.substring("Bearer ".length) ?: return filterChain.doFilter(request, response)
 
+        println("dnjfnakjfndkjnfk")
         // 토큰 검증
         if (jwtUtils.validation(token)) {
-            // 토큰에서 username 파싱
-            val username = jwtUtils.parseUsername(token)
-            // username으로 AuthenticationToken 생성
-            val authentication: Authentication = jwtUtils.getAuthentication(username)
+            println("검증됨")
+            // 토큰에서 userId 파싱
+            val userId = jwtUtils.parseUserId(token)
+            // userId로 AuthenticationToken 생성
+            val authentication: Authentication = jwtUtils.getAuthentication(userId)
             // 생성된 AuthenticationToken을 SecurityContext가 관리하도록 설정
             SecurityContextHolder.getContext().authentication = authentication
         }
