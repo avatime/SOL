2102a534d74dd1adcfd7c02335088bbcec59391f
정건우 <abcxj123@naver.com>
2022-10-27 17:51:37 +0900

    ﻿feat(main) : 메인화면 스크린 추가

--- android/app/src/main/java/com/finance/android/ui/screens/HomeScreen.kt
+++ android/app/src/main/java/com/finance/android/ui/screens/HomeScreen.kt
@@ -1,41 +1,52 @@
 package com.finance.android.ui.screens
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.finance.android.R
+import com.finance.android.ui.components.ButtonType
+import com.finance.android.ui.components.RoundedTextButton
+import com.finance.android.ui.components.TextButton
 
 @Preview
 @Composable
 fun HomeScreen() {
-    CardContainer(modifier = Modifier
-        .fillMaxWidth()
-        .padding(
-            top = dimensionResource(R.dimen.padding_medium),
-            start = dimensionResource(R.dimen.padding_medium),
-            end = dimensionResource(R.dimen.padding_medium)
+    Column (modifier = Modifier
+        .verticalScroll(rememberScrollState())
+        .background(color = Color(R.color.light_gray))) {
+        CardContainer(modifier = Modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_medium))
+            .background(color = Color.White, shape = RoundedCornerShape(10))
+            .height(350.dp)
         )
-        .background(color = Color.Gray, shape = RoundedCornerShape(15))
-        .height(200.dp)
+        CardContainer2(modifier = Modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_medium))
+            .background(color = Color.White, shape = RoundedCornerShape(10))
+            .height(350.dp)
         )
+    }
 }
 
 @Composable
@@ -47,12 +58,142 @@ fun CardContainer(modifier: Modifier) {
             Text(
                 text = "자산",
                 fontWeight = FontWeight.Bold,
-                fontSize = 24.sp
+                fontSize = 20.sp
+            )
+            Text(text = "4", color = Color.Gray, modifier = Modifier.padding(start = 8.dp))
+            Spacer(modifier = Modifier.weight(1.0f))
+            IconButton(onClick = { /*TODO*/ }, modifier = Modifier.size(30.dp)) {
+                Image(painter = painterResource(R.drawable.arrow_forward_ios),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp, bottom = 8.dp),
+                    contentScale = ContentScale.FillWidth
+                )
+            }
+        }
+        AccountListItem()
+        AccountListItem()
+        AccountListItem()
+        Image(painter = painterResource(R.drawable.line),
+            contentDescription = null,
+            modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
+        )
+        InsuranceListItem()
+    }
+}
+
+@Composable
+fun CardContainer2 (modifier: Modifier) {
+    Column(modifier = modifier
+        .padding(dimensionResource(R.dimen.padding_medium))
+    ) {
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = "쌀",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
             )
             Spacer(modifier = Modifier.weight(1.0f))
-            IconButton(onClick = { /*TODO*/ }, modifier = Modifier.size(20.dp)) {
-                Image(painter = painterResource(R.drawable.arrow_forward_ios), contentDescription = null)
+            IconButton(onClick = { /*TODO*/ }, modifier = Modifier.size(30.dp)) {
+                Image(painter = painterResource(R.drawable.arrow_forward_ios),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp, bottom = 8.dp),
+                    contentScale = ContentScale.FillWidth
+                )
             }
+        }
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(painter = painterResource(R.drawable.ssal),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(150.dp)
+                    .width(150.dp)
+            )
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = dimensionResource(R.dimen.padding_medium)),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextButton(onClick = { /*TODO*/ },
+                    text = "만보기",
+                    modifier = Modifier
+                        .height(40.dp)
+                        .width(120.dp)
+                        .padding(start = 20.dp),
+                    buttonType = ButtonType.CIRCULAR,
+                )
+                Spacer(modifier = Modifier.weight(1.0f))
+                TextButton(onClick = { /*TODO*/ },
+                    text = "불러오기",
+                    modifier = Modifier
+                        .height(40.dp)
+                        .width(120.dp)
+                        .padding(end = 20.dp),
+                    buttonType = ButtonType.CIRCULAR,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun AccountListItem() {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = dimensionResource(R.dimen.padding_medium)),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("https://www.shinhancard.com/pconts/company/images/contents/shc_symbol_ci.png")
+                .crossfade(true)
+                .build(),
+            contentDescription = null,
+            modifier = Modifier.size(40.dp)
+        )
+        Column (modifier = Modifier
+            .padding(start = 8.dp)) {
+            Text(text = "나라사랑행복계좌")
+            Text(text = "10,000원", fontWeight = FontWeight.Bold)
+        }
+        Spacer(modifier = Modifier.weight(1.0f))
+        TextButton(onClick = { /*TODO*/ },
+            text = "송금",
+            modifier = Modifier
+                .height(30.dp)
+                .width(70.dp),
+            buttonType = ButtonType.ROUNDED,
+            fontSize = 10.sp
+        )
+    }
+}
+
+@Composable
+fun InsuranceListItem() {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = dimensionResource(R.dimen.padding_medium)),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("https://www.shinhancard.com/pconts/company/images/contents/shc_symbol_ci.png")
+                .crossfade(true)
+                .build(),
+            contentDescription = null,
+            modifier = Modifier.size(40.dp)
+        )
+        Column (modifier = Modifier
+            .padding(start = 8.dp)) {
+            Text(text = "신종단체상해보험", fontSize = 12.sp)
+            Text(text = "보험료: 10,000원", fontWeight = FontWeight.Bold)
+            Text(text = "계약자: 홍길동|피보험자: 홍길동", fontSize = 10.sp)
         }
     }
 }
--- android/app/src/main/java/com/finance/android/utils/ext/Modifier.kt
+++ android/app/src/main/java/com/finance/android/utils/ext/Modifier.kt
@@ -1,8 +1,6 @@
 package com.finance.android.utils.ext
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.res.dimensionResource
