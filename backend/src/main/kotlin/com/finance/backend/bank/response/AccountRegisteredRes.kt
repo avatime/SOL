6bb96bbea2d17b9f7a32e2bfe4ba9c48670851d3
이지나 <dlwlsk0428@naver.com>
2022-10-28 18:58:36 +0900

    feat: 메인에 표시할 계좌 및 보험

--- backend/src/main/kotlin/com/finance/backend/bank/response/AccountRegisteredRes.kt
+++ backend/src/main/kotlin/com/finance/backend/bank/response/AccountRegisteredRes.kt
@@ -1,10 +1,12 @@
 package com.finance.backend.bank.response
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.finance.backend.insurance.response.MyInsuranceInfoDetailRes
+import com.finance.backend.insurance.response.MyInsuranceInfoRes
 
 class AccountRegisteredRes(
         accountList: List<BankAccountRes>,
-        insuranceList: List<String>,
+        insuranceList: List<MyInsuranceInfoDetailRes>,
         financeList: List<BankAccountRes>
 ) {
     @JsonProperty("account_list")
--- backend/src/main/kotlin/com/finance/backend/insurance/repository/InsuranceRepository.kt
+++ backend/src/main/kotlin/com/finance/backend/insurance/repository/InsuranceRepository.kt
@@ -3,9 +3,10 @@ package com.finance.backend.insurance.repository
 import com.finance.backend.insurance.entity.Insurance
 import com.finance.backend.user.User
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface InsuranceRepository : JpaRepository<Insurance, Long> {
     fun findByIdAndUserAndType(id : Long, user : User, type : Int) : Insurance?
     fun findAllByUserAndType(user : User, type : Int) : List<Insurance>
-    fun findAllByUserAndIsRegAndType(user: User, isReg : Boolean, type : Int) : List<Insurance>
+    fun findAllByUserIdAndIsRegAndType(userid: UUID, isReg : Boolean, type : Int) : List<Insurance>
 }
