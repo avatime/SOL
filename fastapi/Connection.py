d26b390c523f178135f87d2f95a6d9f163650a67
민경욱 <rrkcl7733@naver.com>
2022-10-28 17:57:59 +0900

    feat: .env로 mysql 계정 암호화

--- fastapi/Connection.py
+++ fastapi/Connection.py
@@ -1,9 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from pydantic import BaseSettings
 
 
-URL = "mysql+mysqlconnector://ssafy:wkdbf77!@k7a403.p.ssafy.io:3306/tmp"
+class Settings(BaseSettings):
+    USERNAME: str
+    PASSWORD: str
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
+URL = f"mysql+mysqlconnector://{settings.USERNAME}:{settings.PASSWORD}!@k7a403.p.ssafy.io:3306/tmp"
 engine = create_engine(URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
