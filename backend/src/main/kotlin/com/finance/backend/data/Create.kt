a3f651663546aaa6185d23caa9d471465997e792
letgodchan0 <letgodchan0@gmail.com>
2022-10-31 18:44:36 +0900

    feat: 이체 내역 데이터 생성

--- backend/src/main/kotlin/com/finance/backend/data/Create.kt
+++ backend/src/main/kotlin/com/finance/backend/data/Create.kt
@@ -3,6 +3,9 @@ package com.finance.backend.data
 import com.finance.backend.bank.Account
 import com.finance.backend.bank.AccountRepository
 import com.finance.backend.data.request.AccountReq
+import com.finance.backend.data.request.TradeReq
+import com.finance.backend.tradeHistory.TradeHistory
+import com.finance.backend.tradeHistory.TradeHistoryRepository
 import com.finance.backend.user.UserRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -17,7 +20,8 @@ import kotlin.reflect.typeOf
 @RequestMapping("/api/v1/data")
 class Create(
         val accountRepository: AccountRepository,
-        val userRepository: UserRepository
+        val userRepository: UserRepository,
+        val tradeHistoryRepository: TradeHistoryRepository
 ) {
 
     @PostMapping("/account")
@@ -29,5 +33,21 @@ class Create(
         return ResponseEntity
                 .ok()
                 .body(accountRepository.save(account))
+    }
+
+    @PostMapping("/trade")
+    fun postTrade(@RequestBody tradeReq: TradeReq): ResponseEntity<Any>{
+        val accountSend = accountRepository.findById(tradeReq.acNo).get()
+        val tradeSend = TradeHistory(tradeReq.tdVal, LocalDateTime.now(), 2, tradeReq.tdTg, tradeReq.tdTgAc, tradeReq.tdRec, tradeReq.tdSed, accountSend)
+
+        val accountReceived = accountRepository.findById(tradeReq.tdTgAc).get()
+        val tradeReceived = TradeHistory(tradeReq.tdVal, LocalDateTime.now(), 1, accountSend.acName, tradeReq.acNo, tradeReq.tdSed, tradeReq.tdRec, accountReceived)
+
+        tradeHistoryRepository.save(tradeSend)
+        tradeHistoryRepository.save(tradeReceived)
+
+        return ResponseEntity
+                .ok()
+                .body("거래 완료")
     }
 }
