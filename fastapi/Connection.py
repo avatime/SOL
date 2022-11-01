484385bbe1fba0af8a492f59b27234bf10fc9587
민경욱 <rrkcl7733@naver.com>
2022-11-01 11:27:29 +0900

    ﻿feat: 계좌 랜덤 3개 생성

--- fastapi/Connection.py
+++ fastapi/Connection.py
@@ -13,7 +13,8 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-URL = f"mysql+mysqlconnector://{settings.USERNAME.lower()}:{settings.PASSWORD}@k7a403.p.ssafy.io:3306/tmp"
-engine = create_engine(URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+URL = f"mysql+mysqlconnector://{settings.USERNAME.lower()}:{settings.PASSWORD}@k7a403.p.ssafy.io:3306/finance_db"
+engine = create_engine(URL, echo=True)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False)
 Base = declarative_base()
