e1344e217ad32d8a35b2a92f76f8cef443e6ea59
letgodchan0 <letgodchan0@gmail.com>
2022-11-01 10:24:09 +0900

    feat: 북마크 / 최근 거래 계좌 조회

--- backend/src/main/kotlin/com/finance/backend/bank/response/RecentTradeRes.kt
+++ backend/src/main/kotlin/com/finance/backend/bank/response/RecentTradeRes.kt
@@ -3,16 +3,20 @@ package com.finance.backend.bank.response
 import com.fasterxml.jackson.annotation.JsonProperty
 
 class RecentTradeRes(
-        acName : String,
+        acReceive : String,
         acNo : String,
+        cpName : String,
         bkStatus: Boolean,
         cpLogo: String
 ) {
-    @JsonProperty("ac_name")
-    val acName: String = acName
+    @JsonProperty("ac_receive")
+    val acReceive: String = acReceive
 
     @JsonProperty("ac_no")
     val acNo: String = acNo
+
+    @JsonProperty("cp_name")
+    val cpName: String = cpName
 
     @JsonProperty("bk_status")
     val bkStatus: Boolean = bkStatus
