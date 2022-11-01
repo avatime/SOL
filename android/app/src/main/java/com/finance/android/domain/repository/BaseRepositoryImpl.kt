3ee3f2ae6c921f42b9a85758cfe9e99a8cb2bbc8
이주영 <dlendjs93@gmail.com>
2022-11-01 13:23:41 +0900

    ﻿feat: 지문 인식 기능 추가 및 403 에러 처리 로직 수정

--- android/app/src/main/java/com/finance/android/domain/repository/BaseRepositoryImpl.kt
+++ android/app/src/main/java/com/finance/android/domain/repository/BaseRepositoryImpl.kt
@@ -2,44 +2,11 @@ package com.finance.android.domain.repository
 
 import com.finance.android.domain.dto.response.ReissueTokenResponseDto
 import com.finance.android.domain.service.BaseService
-import android.util.Log
-import com.finance.android.domain.RetrofitClient
-import com.finance.android.utils.Response
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import retrofit2.HttpException
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-open class BaseRepositoryImpl @Inject constructor(
+class BaseRepositoryImpl @Inject constructor(
     private val baseService: BaseService
 ) : BaseRepository {
-    protected suspend fun <T> run(call: suspend () -> T) = flow {
-        var flag = false
-
-        suspend fun load() {
-            emit(Response.Loading)
-            try {
-                emit(Response.Success(call()))
-            } catch (e: HttpException) {
-                if (e.code() == 403 && !flag) {
-                    flag = true
-                    val accessToken = reissueToken().accessToken
-                    RetrofitClient.resetAccessToken(accessToken)
-                    load()
-                } else {
-                    emit(Response.Failure(e))
-                }
-            } catch (e: Exception) {
-                Log.e("SOLSOL", e.stackTraceToString())
-            }
-        }
-
-        load()
-    }.flowOn(Dispatchers.IO)
-
     override suspend fun reissueToken(): ReissueTokenResponseDto {
         return baseService.refreshToken();
     }
