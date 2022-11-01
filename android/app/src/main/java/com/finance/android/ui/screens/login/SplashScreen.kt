92b93d9398aa67c74e3c2f3b2c10fb43a9c110a7
이지나 <dlwlsk0428@naver.com>
2022-11-01 15:27:23 +0900

    feat: gif 추가 - 미완성

--- android/app/src/main/java/com/finance/android/ui/screens/login/SplashScreen.kt
+++ android/app/src/main/java/com/finance/android/ui/screens/login/SplashScreen.kt
@@ -1,11 +1,25 @@
 package com.finance.android.ui.screens.login
 
+import android.media.Image
+import android.os.Build.VERSION.SDK_INT
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.Role.Companion.Image
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
+import coil.size.Size
+import com.bumptech.glide.Glide
+import com.finance.android.R
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen() {
@@ -13,6 +27,24 @@ fun SplashScreen() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "SplashScreen")
+        val context = LocalContext.current
+        val imageLoader = ImageLoader.Builder(context)
+            .components {
+                if (SDK_INT >= 28) {
+                    add(ImageDecoderDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
+            }
+            .build()
+        Image(
+            painter = rememberAsyncImagePainter(
+                ImageRequest.Builder(context).data(data = R.drawable.finance_splash).apply(block = {
+                    size(Size.ORIGINAL)
+                }).build(), imageLoader = imageLoader
+            ),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+        )
     }
 }
