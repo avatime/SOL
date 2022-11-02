eeefa5df7f64f8420915970de68230e485757dda
이지나 <dlwlsk0428@naver.com>
2022-11-02 19:11:22 +0900

    ﻿feat : 출석체크

---
+++ android/app/src/main/java/com/finance/android/ui/screens/more/MoreScreen.kt
@@ -0,0 +1,28 @@
+package com.finance.android.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.finance.android.utils.ext.withBottomButton
+
+
+@Composable
+fun MoreScreen(navController: NavController) {
+
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+        Button(
+            onClick = { },
+            modifier = Modifier.withBottomButton()
+        ) {
+            Text("여기는 MoreScreen 입니다람쥐")
+        }
+    }
+}
