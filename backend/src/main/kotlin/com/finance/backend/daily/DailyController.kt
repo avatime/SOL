55502d51ee9611250c7749003e895b1a74839f9e
이지나 <dlwlsk0428@naver.com>
2022-11-02 20:51:25 +0900

    feat : 로직 수정

--- backend/src/main/kotlin/com/finance/backend/daily/DailyController.kt
+++ backend/src/main/kotlin/com/finance/backend/daily/DailyController.kt
@@ -12,56 +12,26 @@ class DailyController(private val dailyService: DailyService) {
 
     @PostMapping("")
     fun check(@RequestHeader("access_token") accessToken : String) : ResponseEntity<Any?> {
-        return try{
-            ResponseEntity.status(200).body(dailyService.check(accessToken))
-        } catch (e : TokenExpiredException) {
-            ResponseEntity.status(403).body("Token Expired")
-        } catch (e : InvalidUserException) {
-            ResponseEntity.status(409).body("Cannot Found User")
-        } catch (e : Exception) {
-            ResponseEntity.status(500).body("Internal Server Error")
-        }
+        return ResponseEntity.status(200).body(dailyService.check(accessToken))
     }
 
     @GetMapping("/{year}/{month}")
     fun getAttendance(@RequestHeader("access_token") accessToken : String,@PathVariable year: Int, @PathVariable month: Int) : ResponseEntity<Any?> {
-        return try{
-            ResponseEntity.status(200).body(dailyService.getAttendance(accessToken, year, month))
-        } catch (e : TokenExpiredException) {
-            ResponseEntity.status(403).body("Token Expired")
-        } catch (e : InvalidUserException) {
-            ResponseEntity.status(409).body("Cannot Found User")
-        } catch (e : Exception) {
-            println(e)
-            ResponseEntity.status(500).body("Internal Server Error")
-        }
+        return ResponseEntity.status(200).body(dailyService.getAttendance(accessToken, year, month))
     }
 
     @PostMapping("/walk")
     fun walk(@RequestHeader("access_token") accessToken : String, @RequestBody walkDto: WalkDto) : ResponseEntity<Any?> {
-        return try{
-            ResponseEntity.status(200).body(dailyService.walk(accessToken, walkDto.walk))
-        } catch (e : TokenExpiredException) {
-            ResponseEntity.status(403).body("Token Expired")
-        } catch (e : InvalidUserException) {
-            ResponseEntity.status(409).body("Cannot Found User")
-        } catch (e : Exception) {
-            println(e)
-            ResponseEntity.status(500).body("Internal Server Error")
-        }
+        return ResponseEntity.status(200).body(dailyService.walk(accessToken, walkDto.walk))
     }
 
     @GetMapping("/walk/{year}/{month}")
     fun getWalk(@RequestHeader("access_token") accessToken : String, @PathVariable year: Int, @PathVariable month: Int): ResponseEntity<Any?> {
-        return try{
-            ResponseEntity.status(200).body(dailyService.getWalk(accessToken, year, month))
-        } catch (e : TokenExpiredException) {
-            ResponseEntity.status(403).body("Token Expired")
-        } catch (e : InvalidUserException) {
-            ResponseEntity.status(409).body("Cannot Found User")
-        } catch (e : Exception) {
-            println(e)
-            ResponseEntity.status(500).body("Internal Server Error")
-        }
+        return ResponseEntity.status(200).body(dailyService.getWalk(accessToken, year, month))
+    }
+
+    @GetMapping("")
+    fun test(@RequestHeader("access_token") accessToken : String) : ResponseEntity<Any?> {
+        return ResponseEntity.status(200).body("Success")
     }
 }
