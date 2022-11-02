1a3473a4a72d3629deaadb645d20f353d35dafb0
이주영 <dlendjs93@gmail.com>
2022-11-02 09:46:58 +0900

    refactor: BankRepository return 형 변경

--- android/app/src/main/java/com/finance/android/domain/repository/BankRepository.kt
+++ android/app/src/main/java/com/finance/android/domain/repository/BankRepository.kt
@@ -1,19 +1,25 @@
 package com.finance.android.domain.repository
 
 import com.finance.android.domain.dto.response.*
-import com.finance.android.utils.Response
-import kotlinx.coroutines.flow.Flow
 import retrofit2.http.Field
 
 interface BankRepository {
-    suspend fun getAccountList() : Flow<Response<MutableList<BankAccountResponseDto>>>
-    suspend fun putRegisterAccount(@Field("ac_no")acNo:String) : Flow<Response<Unit>>
-    suspend fun putRegisterMainAccount(@Field("ac_no") acNo: String) : Flow<Response<Unit>>
-    suspend fun putBookmarkAccount(@Field("ac_no") acNo: String) : Flow<Response<Unit>>
-    suspend fun getAccountSendDetail(@Field("an_no") acNo: String, @Field("type") type:Int) :Flow<Response<MutableList<BankTradeResponseDto>>>
-    suspend fun getAccountDetail(@Field("ac_no") acNo: String) : Flow<Response<MutableList<BankDetailResponseDto>>>
-    suspend fun getRecentAccount() : Flow<Response<MutableList<RecentTradeResponseDto>>>
-    suspend fun checkAccount(@Field("ac_no")acNo: String, @Field("cp_code")cdCode : Int) : Flow<Response<String>>
-    suspend fun getAllBankAccount() : Flow<Response<MutableList<BankInfoResponseDto>>>
-    suspend fun getAllMainAccount() : Flow<Response<AccountRegisteredResponseDto>>
+    suspend fun getAccountList(): MutableList<BankAccountResponseDto>
+    suspend fun putRegisterAccount(@Field("ac_no") acNo: String)
+    suspend fun putRegisterMainAccount(@Field("ac_no") acNo: String)
+    suspend fun putBookmarkAccount(@Field("ac_no") acNo: String)
+    suspend fun getAccountSendDetail(
+        @Field("an_no") acNo: String,
+        @Field("type") type: Int
+    ): MutableList<BankTradeResponseDto>
+
+    suspend fun getAccountDetail(@Field("ac_no") acNo: String): MutableList<BankDetailResponseDto>
+    suspend fun getRecentAccount(): MutableList<RecentTradeResponseDto>
+    suspend fun checkAccount(
+        @Field("ac_no") acNo: String,
+        @Field("cp_code") cdCode: Int
+    ): String
+
+    suspend fun getAllBankAccount(): MutableList<BankInfoResponseDto>
+    suspend fun getAllMainAccount(): AccountRegisteredResponseDto
 }
