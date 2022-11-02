14b42a625ec8060b8f9e90338a2811c0d359452c
이지나 <dlwlsk0428@naver.com>
2022-11-02 13:52:21 +0900

    fix : splash 오류 수정

--- android/app/src/main/java/com/finance/android/ui/screens/login/SplashScreen.kt
+++ android/app/src/main/java/com/finance/android/ui/screens/login/SplashScreen.kt
@@ -1,16 +1,13 @@
 package com.finance.android.ui.screens.login
 
-import android.media.Image
 import android.os.Build.VERSION.SDK_INT
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.semantics.Role.Companion.Image
 import coil.ImageLoader
 import coil.compose.rememberAsyncImagePainter
 import coil.decode.GifDecoder
@@ -18,9 +15,7 @@ import coil.decode.ImageDecoderDecoder
 import coil.request.ImageRequest
 import coil.request.repeatCount
 import coil.size.Size
-import com.bumptech.glide.Glide
 import com.finance.android.R
-import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen() {
