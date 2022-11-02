cb92f9f4688feb36778172c8d353f071c0eefad1
letgodchan0 <letgodchan0@gmail.com>
2022-11-03 06:04:15 +0900

    feat: 내 계좌 조회 (북마크)

--- backend/src/main/kotlin/com/finance/backend/bank/AccountService.kt
+++ backend/src/main/kotlin/com/finance/backend/bank/AccountService.kt
@@ -13,7 +13,7 @@ interface AccountService {
     fun registerBookmarkAccount(acNo: String, token: String)
     fun getAccountDetail(acNo: String): BankDetailRes
     fun getAccountDetailType(acNo: String, type: Int): List<BankTradeRes>
-    fun getRecentTrade(token: String): List<RecentTradeRes>
+    fun getRecentTrade(token: String): List<RecentMyTradeRes>
     fun getUserName(acNo: String, cpCode: Long): String
     fun getBankInfo():List<BankInfoRes>
     fun getAccountRegistered(token: String): AccountRegisteredRes
