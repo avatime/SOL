ddf94ba0b08a52eb034d1364b8af31c5126e9aea
민경욱 <rrkcl7733@naver.com>
2022-11-02 14:15:34 +0900

    refactor: 예시 코드 삭제

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, Response, status
-from starlette.responses import JSONResponse
 from fastapi.params import Depends
 # from starlette.responses import RedirectResponse
 import models, schemas
@@ -26,11 +25,6 @@ def get_db():
 #     return RedirectResponse("/docs/")
 
 
-@app.get("/hello/{name}", name="제목입니다", description="문장입니다")
-async def say_hello(name: str):
-    return JSONResponse({"message": f"Hello {name}"})
-
-
 @app.post("/data/v1/user/register", status_code=200)
 async def test(user: schemas.userReq, response: Response, db: Session = Depends(get_db)):
     try:
@@ -38,7 +32,3 @@ async def test(user: schemas.userReq, response: Response, db: Session = Depends(
         function.create(user_id, db)
     except:
         response.status_code = status.HTTP_409_CONFLICT
-
-@app.get("/data/v1/random")
-async def a():
-    return JSONResponse({"message": "dddddddddd"})
