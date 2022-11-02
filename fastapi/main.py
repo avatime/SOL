f859d7eec38925b17a9e9d0eaae55cffd2c69306
민경욱 <rrkcl7733@naver.com>
2022-11-02 10:01:10 +0900

    feat: redirecct swagger 수정

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
 from fastapi.params import Depends
-from starlette.responses import RedirectResponse
+# from starlette.responses import RedirectResponse
 import models, schemas
 from Connection import SessionLocal, engine
 from sqlalchemy.orm import Session
@@ -21,9 +21,9 @@ def get_db():
         db.close()
 
 
-@app.get("/")
-async def main():
-    return RedirectResponse("/docs/")
+# @app.get("/")    # 스웨거 확인용
+# async def main():
+#     return RedirectResponse("/docs/")
 
 
 @app.get("/hello/{name}", name="제목입니다", description="문장입니다")
@@ -31,7 +31,7 @@ async def say_hello(name: str):
     return JSONResponse({"message": f"Hello {name}"})
 
 
-@app.post("/user/register")
+@app.post("/data/v1/user/register")
 async def test(user: schemas.userReq, db: Session = Depends(get_db)):
     # user_id = b'1\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
     function.create(user.user_id, db)
