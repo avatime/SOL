06644443cb942180539cf98d5220482b1c95fc2c
이지나 <dlwlsk0428@naver.com>
2022-11-03 19:37:13 +0900

    feat : 마이페이지

--- android/app/src/main/java/com/finance/android/ui/fragments/AttendanceFragment.kt
+++ android/app/src/main/java/com/finance/android/ui/fragments/AttendanceFragment.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.finance.android.R
