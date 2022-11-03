9ea8c8d18b42cd69c67f80f4245fddd90e48610a
민경욱 <rrkcl7733@naver.com>
2022-11-03 21:16:19 +0900

    feat: phone 송금 kafka 구현

--- backend/src/main/kotlin/com/finance/backend/kafka/KafkaProducer.kt
+++ backend/src/main/kotlin/com/finance/backend/kafka/KafkaProducer.kt
@@ -1,15 +1,21 @@
 package com.finance.backend.kafka
 
 import com.finance.backend.remit.request.RemitInfoReq
+import com.finance.backend.remit.request.RemitPhoneReq
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 
 @Service
-class KafkaProducer(val kafkaTemplate : KafkaTemplate<String, RemitInfoReq>) {
+class KafkaProducer(val kafkaTemplate : KafkaTemplate<String, Any>) {
 
-    private val TOPIC = "remit"
+    private val TOPIC1 = "account"
+    private val TOPIC2 = "phone"
 
-    fun sendMessage(message: RemitInfoReq?) {
-        kafkaTemplate.send(TOPIC, message)
+    fun accountMessage(message: RemitInfoReq?) {
+        kafkaTemplate.send(TOPIC1, message)
+    }
+
+    fun phoneMessage(message: RemitPhoneReq?) {
+        kafkaTemplate.send(TOPIC2, message)
     }
 }
--- backend/src/main/kotlin/com/finance/backend/remit/RemitController.kt
+++ backend/src/main/kotlin/com/finance/backend/remit/RemitController.kt
@@ -24,12 +24,13 @@ class RemitController(val remitService: RemitService, val kafka: KafkaProducer) 
 
     @PostMapping("/account")
     fun postRemit(@RequestBody remitInfoReq: RemitInfoReq): ResponseEntity<Any>{
-        kafka.sendMessage(remitInfoReq)
+        kafka.accountMessage(remitInfoReq)
         return ResponseEntity.status(200).body(remitService.postRemit(remitInfoReq))
     }
 
     @PostMapping("/phone")
     fun postRemitPhone(@RequestBody remitPhoneReq: RemitPhoneReq): ResponseEntity<Any>{
+        kafka.phoneMessage(remitPhoneReq)
         return ResponseEntity
                 .ok()
                 .body(remitService.postRemitPhone(remitPhoneReq))
