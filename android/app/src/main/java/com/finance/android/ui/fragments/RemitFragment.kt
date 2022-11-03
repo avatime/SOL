a7234d2a64f2e11936de597bfa80fde4852ed184
이주영 <dlendjs93@gmail.com>
2022-11-03 10:23:48 +0900

    feat: RemitFragment 에 argument 추가

--- android/app/src/main/java/com/finance/android/ui/fragments/RemitFragment.kt
+++ android/app/src/main/java/com/finance/android/ui/fragments/RemitFragment.kt
@@ -1,29 +1,25 @@
 package com.finance.android.ui.fragments
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.finance.android.ui.components.BackHeaderBar
 import com.finance.android.ui.components.HeaderRemitTabBar
 import com.finance.android.ui.screens.*
-import com.finance.android.utils.Const
 import com.finance.android.viewmodels.RemitViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview
 @Composable
 fun RemitFragment(
-    remitViewModel: RemitViewModel = hiltViewModel()
+    remitViewModel: RemitViewModel = hiltViewModel(),
+    accountName: String,
+    accountNumber: String,
+    balance: Int
 ) {
     val navController = rememberNavController()
 
