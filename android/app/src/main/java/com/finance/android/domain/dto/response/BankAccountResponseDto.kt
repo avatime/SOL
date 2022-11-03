4bf6e98312f0c2d57ab341abec5d142dd2652c14
이주영 <dlendjs93@gmail.com>
2022-11-03 21:10:14 +0900

    fix: 재배치 중 충돌 해결

--- android/app/src/main/java/com/finance/android/domain/dto/response/BankAccountResponseDto.kt
+++ android/app/src/main/java/com/finance/android/domain/dto/response/BankAccountResponseDto.kt
@@ -2,18 +2,18 @@ package com.finance.android.domain.dto.response
 
 import com.google.gson.annotations.SerializedName
 
-data class BankAccountResponseDto (
+data class BankAccountResponseDto(
 
     @SerializedName("ac_no")
-    val acNo : String, //계좌번호
-    val balance : Int, //잔액
+    val acNo: String, //계좌번호
+    val balance: Int, //잔액
     @SerializedName("ac_name")
-    val acName : String, //계좌 이름
+    val acName: String, //계좌 이름
     @SerializedName("cp_name")
-    val cpName : String, //기업명
+    val cpName: String, //기업명
     @SerializedName("cp_logo")
-    val cpLogo : String, //기업 로고 경로
+    val cpLogo: String, //기업 로고 경로
     @SerializedName("ac_reg")
-    val acReg : Boolean // 등록 여부
+    val acReg: Boolean // 등록 여부
 
-    )
+)
