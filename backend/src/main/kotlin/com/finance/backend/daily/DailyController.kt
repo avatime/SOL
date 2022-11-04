b7d57f8717041d921070be427ddda60d33f8058d
이지나 <dlwlsk0428@naver.com>
2022-11-04 17:22:17 +0900

    test : 스케줄러 테스트 2

--- backend/src/main/kotlin/com/finance/backend/daily/DailyController.kt
+++ backend/src/main/kotlin/com/finance/backend/daily/DailyController.kt
@@ -40,7 +40,7 @@ class DailyController(private val dailyService: DailyService) {
     }
 
     @GetMapping("/test/scheduled")
-    @Scheduled(cron = "0 20 8 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 25 17 * * *", zone = "Asia/Seoul")
     fun registFinanceData() : ResponseEntity<Any?> {
         println("동네 사람들~!!!!!!!!!!!!!!!!!!!!!!")
         println("스케줄러 정상 작동해요 " + LocalDateTime.now())
