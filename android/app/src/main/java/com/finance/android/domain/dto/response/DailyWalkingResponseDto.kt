e091889696868b5498a3d62162f58bd23640ed56
정건우 <abcxj123@naver.com>
2022-11-07 17:39:11 +0900

    fix(dto) : SerializedName annotation 추가

--- android/app/src/main/java/com/finance/android/domain/dto/response/DailyWalkingResponseDto.kt
+++ android/app/src/main/java/com/finance/android/domain/dto/response/DailyWalkingResponseDto.kt
@@ -1,9 +1,13 @@
 package com.finance.android.domain.dto.response
 
+import com.google.gson.annotations.SerializedName
 import java.util.Date
 
 data class DailyWalkingResponseDto(
+    @SerializedName("day")
     val day: Date,
+    @SerializedName("success")
     val success: Boolean,
+    @SerializedName("walk")
     val walk : Int
 )
