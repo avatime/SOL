814faa935055353873a0c63306d671d5ddcabe4c
채윤선 <yoonsun0809@gmail.com>
2022-11-07 16:24:49 +0900

    Feat : 틀

---
+++ android/app/src/main/java/com/finance/android/ui/fragments/GroupFragment.kt
@@ -0,0 +1,9 @@
+package com.finance.android.ui.fragments
+
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+fun GroupFragment(navController: NavController,) {
+//    val innerController  = rememberNavController(navigators = )
+
+}
