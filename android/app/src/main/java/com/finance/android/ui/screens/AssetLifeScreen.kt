a00ae086949c3d38302f14e50e0105410a300b9d
정건우 <abcxj123@naver.com>
2022-11-07 10:26:58 +0900

    feat(asset) : 내 자산 계좌 api 연결

--- android/app/src/main/java/com/finance/android/ui/screens/AssetLifeScreen.kt
+++ android/app/src/main/java/com/finance/android/ui/screens/AssetLifeScreen.kt
@@ -19,8 +19,7 @@ import com.finance.android.ui.components.InsuranceListItem
 
 @Composable
 fun AssetLifeScreen(navController: NavController) {
-    Column(modifier = Modifier
-        .padding(top = dimensionResource(R.dimen.padding_small)))
+    Column()
     {
         AssetLifeContainer(modifier = Modifier
             .fillMaxWidth()
