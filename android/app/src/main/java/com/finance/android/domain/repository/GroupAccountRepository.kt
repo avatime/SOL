c4a48945c46c0729a86856e5bbd430f9f27bf73b
채윤선 <yoonsun0809@gmail.com>
2022-11-08 17:59:59 +0900

    temp

--- android/app/src/main/java/com/finance/android/domain/repository/GroupAccountRepository.kt
+++ android/app/src/main/java/com/finance/android/domain/repository/GroupAccountRepository.kt
@@ -1,4 +1,23 @@
 package com.finance.android.domain.repository
 
+import com.finance.android.domain.dto.request.CreateGroupAccountRequestDto
+import com.finance.android.domain.dto.request.GroupDuesRequestDto
+import com.finance.android.domain.dto.request.GroupIdRequestDto
+import com.finance.android.domain.dto.response.DuesResponseDto
+import com.finance.android.domain.dto.response.FriendResponseDto
+import com.finance.android.domain.dto.response.PublicAccountResponseDto
+import com.finance.android.domain.dto.response.PublicTradeResponseDto
+import retrofit2.http.Body
+
 interface GroupAccountRepository {
+    suspend fun getGroupAccount(): MutableList<PublicAccountResponseDto>
+    suspend fun postMakeGroupAccount(@Body createGroupAccountRequestDto: CreateGroupAccountRequestDto)
+    suspend fun putDeleteGroupAccount(@Body groupIdRequestDto: GroupIdRequestDto)
+    suspend fun postTradeHistory(@Body groupIdRequestDto: GroupIdRequestDto) : MutableList<PublicTradeResponseDto>
+    suspend fun postGroupMember(@Body groupIdRequestDto: GroupIdRequestDto) : MutableList<FriendResponseDto>
+    suspend fun postDuesHistory(@Body groupIdRequestDto: GroupIdRequestDto) : MutableList<DuesResponseDto>
+    suspend fun postDuesHistoryDetail(@Body groupDuesRequestDto: GroupDuesRequestDto) : MutableList<DuesResponseDto>
+    suspend fun postPayDues(@Body remitDuesResponseDto: DuesResponseDto)
+    suspend fun postRegistDues(@Body createGroupAccountRequestDto: CreateGroupAccountRequestDto)
+    suspend fun putDeleteDues(@Body groupDuesRequestDto: GroupDuesRequestDto)
 }
--- android/app/src/main/java/com/finance/android/domain/service/GroupAccountService.kt
+++ android/app/src/main/java/com/finance/android/domain/service/GroupAccountService.kt
@@ -1,4 +1,46 @@
 package com.finance.android.domain.service
 
+import com.finance.android.domain.dto.request.CreateGroupAccountRequestDto
+import com.finance.android.domain.dto.request.GroupDuesRequestDto
+import com.finance.android.domain.dto.request.GroupIdRequestDto
+import com.finance.android.domain.dto.response.DuesResponseDto
+import com.finance.android.domain.dto.response.FriendResponseDto
+import com.finance.android.domain.dto.response.PublicAccountResponseDto
+import com.finance.android.domain.dto.response.PublicTradeResponseDto
+import com.finance.android.utils.Const
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+
 interface GroupAccountService {
+    @GET("${Const.API_PATH}/public")//모임 통장 조회
+    suspend fun getGroupAccount(): MutableList<PublicAccountResponseDto>
+
+    @POST("${Const.API_PATH}/public")//모임 통장 생성
+    suspend fun postMakeGroupAccount(@Body createGroupAccountRequestDto: CreateGroupAccountRequestDto)
+
+    @PUT("${Const.API_PATH}/public")// 모임 통장 제거
+    suspend fun putDeleteGroupAccount(@Body groupIdRequestDto: GroupIdRequestDto)
+
+    @POST("${Const.API_PATH}/public/trade")//입출금 내역 조회
+    suspend fun postTradeHistory(@Body groupIdRequestDto: GroupIdRequestDto) : MutableList<PublicTradeResponseDto>
+
+    @POST("${Const.API_PATH}/public/friend")// 모임 친구 조회
+    suspend fun postGroupMember(@Body groupIdRequestDto: GroupIdRequestDto) : MutableList<FriendResponseDto>
+
+    @POST("${Const.API_PATH}/public/dues")// 회비 전체 조회
+    suspend fun postDuesHistory(@Body groupIdRequestDto: GroupIdRequestDto) : MutableList<DuesResponseDto>
+
+    @POST("${Const.API_PATH}/public/dues/detail") //회비 상세 조회
+    suspend fun postDuesHistoryDetail(@Body groupDuesRequestDto: GroupDuesRequestDto) : MutableList<DuesResponseDto>
+
+    @POST("${Const.API_PATH}/public/dues/pay") // 회비 입금
+    suspend fun postPayDues(@Body remitDuesResponseDto: DuesResponseDto)
+
+    @POST("${Const.API_PATH}/public/dues/regist") //회비 생성
+    suspend fun postRegistDues(@Body createGroupAccountRequestDto: CreateGroupAccountRequestDto)
+
+    @PUT("${Const.API_PATH}/public/dues") //회비 비활성화
+    suspend fun putDeleteDues(@Body groupDuesRequestDto: GroupDuesRequestDto)
 }
