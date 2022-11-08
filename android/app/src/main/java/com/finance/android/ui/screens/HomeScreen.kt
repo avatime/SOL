f3adc4d2dfe09df919264bc5a2ec6be588de9cc6
이주영 <dlendjs93@gmail.com>
2022-11-08 18:05:54 +0900

    fix: 병합 중 누락된 부분 추가

--- android/app/src/main/java/com/finance/android/ui/screens/HomeScreen.kt
+++ android/app/src/main/java/com/finance/android/ui/screens/HomeScreen.kt
@@ -2,7 +2,11 @@ package com.finance.android.ui.screens
 
 import android.Manifest
 import android.app.NotificationManager
+import android.app.Service
 import android.content.Context
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.net.Uri
 import android.os.Build
 import androidx.compose.foundation.*
@@ -12,6 +16,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,13 +30,16 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavController
 import com.finance.android.R
 import com.finance.android.domain.dto.response.AccountRegisteredResponseDto
+import com.finance.android.services.WalkService
 import com.finance.android.ui.components.*
+import com.finance.android.ui.theme.Disabled
 import com.finance.android.utils.Const
 import com.finance.android.utils.Response
 import com.finance.android.viewmodels.HomeViewModel
@@ -69,7 +77,11 @@ fun HomeScreen(
 
     Scaffold(
         topBar = {
-            TopBar(navController = navController)
+            TopBar(
+                navController = navController,
+                homeViewModel = homeViewModel
+
+            )
         },
         containerColor = MaterialTheme.colorScheme.background
     ) {
@@ -180,7 +192,7 @@ private fun HomeCardContainer(
                 cardName = it.cardName,
                 cardImgPath = it.cardImgPath,
                 onClickItem = {
-                    navController.navigate("${Const.Routes.CARD_DETAIL}/${it.cardNumber}/${pathTmp}/${it.cardName}")
+                    navController.navigate("${Const.Routes.CARD_DETAIL}/${it.cardNumber}/$pathTmp/${it.cardName}")
                 }
             )
         }
@@ -287,7 +299,8 @@ private fun HomeCardContainer2(modifier: Modifier, navController: NavController)
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 private fun TopBar(
-    navController: NavController
+    navController: NavController,
+    homeViewModel: HomeViewModel
 ) {
     Row(
         modifier = Modifier
@@ -324,7 +337,8 @@ private fun TopBar(
         }
 
         PedometerOnStateButton(
-            onClick = onClick
+            onClick = onClick,
+            homeViewModel = homeViewModel
         )
     }
 }
@@ -361,8 +375,45 @@ private fun PedometerOffStateButton(
 @Composable
 private fun PedometerOnStateButton(
     modifier: Modifier = Modifier,
-    onClick: () -> Unit = {}
+    onClick: () -> Unit = {},
+    homeViewModel: HomeViewModel = hiltViewModel()
 ) {
+    val context = LocalContext.current
+    val lifecycleOwner = rememberUpdatedState(LocalLifecycleOwner.current)
+
+    LaunchedEffect(Unit) {
+        val serviceIntent = Intent(context, WalkService::class.java)
+        ContextCompat.startForegroundService(context, serviceIntent)
+    }
+
+    DisposableEffect(lifecycleOwner.value) {
+        val lifecycle = lifecycleOwner.value.lifecycle
+        val observer = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_RESUME -> {
+                    val sensorManager =
+                        context.getSystemService(Service.SENSOR_SERVICE) as SensorManager
+                    sensorManager.registerListener(
+                        homeViewModel,
+                        sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER),
+                        SensorManager.SENSOR_DELAY_GAME
+                    )
+                }
+                Lifecycle.Event.ON_PAUSE -> {
+                    (context.getSystemService(Service.SENSOR_SERVICE) as SensorManager).unregisterListener(
+                        homeViewModel
+                    )
+                }
+                else -> {}
+            }
+        }
+
+        lifecycle.addObserver(observer)
+        onDispose {
+            lifecycle.removeObserver(observer)
+        }
+    }
+
     Row(
         modifier = modifier
             .clip(RoundedCornerShape(10.dp))
@@ -373,8 +424,23 @@ private fun PedometerOnStateButton(
             .padding(10.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            text = "만보기 ON 상태다!!"
-        )
+        homeViewModel.walkCount.value?.let {
+            Box(
+                modifier = Modifier.size(24.dp)
+            ) {
+                CircularProgressIndicator(
+                    progress = 1f,
+                    color = Disabled
+                )
+                CircularProgressIndicator(
+                    progress = it.toFloat() / 5000,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+            Spacer(modifier = Modifier.width(7.dp))
+            Text(
+                text = "$it 걸음"
+            )
+        }
     }
 }
