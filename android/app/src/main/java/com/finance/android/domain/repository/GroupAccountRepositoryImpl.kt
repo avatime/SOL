cd624909f333dce2b8dfbba1335801913fc7c739
채윤선 <yoonsun0809@gmail.com>
2022-11-08 17:59:59 +0900

    temp

--- android/app/src/main/java/com/finance/android/domain/repository/GroupAccountRepositoryImpl.kt
+++ android/app/src/main/java/com/finance/android/domain/repository/GroupAccountRepositoryImpl.kt
@@ -1,9 +1,57 @@
 package com.finance.android.domain.repository
 
+import com.finance.android.domain.dto.request.CreateGroupAccountRequestDto
+import com.finance.android.domain.dto.request.GroupDuesRequestDto
+import com.finance.android.domain.dto.request.GroupIdRequestDto
+import com.finance.android.domain.dto.response.DuesResponseDto
+import com.finance.android.domain.dto.response.FriendResponseDto
+import com.finance.android.domain.dto.response.PublicAccountResponseDto
+import com.finance.android.domain.dto.response.PublicTradeResponseDto
 import com.finance.android.domain.service.GroupAccountService
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class GroupAccountRepositoryImpl @Inject constructor(
     private val groupAccountService: GroupAccountService
 ) : GroupAccountRepository {
+    override suspend fun getGroupAccount(): MutableList<PublicAccountResponseDto> {
+        return groupAccountService.getGroupAccount()
+    }
+
+    override suspend fun postMakeGroupAccount(createGroupAccountRequestDto: CreateGroupAccountRequestDto) {
+        return groupAccountService.postMakeGroupAccount(createGroupAccountRequestDto)
+    }
+
+    override suspend fun putDeleteGroupAccount(groupIdRequestDto: GroupIdRequestDto) {
+        return groupAccountService.putDeleteGroupAccount(groupIdRequestDto)
+    }
+
+    override suspend fun postTradeHistory(groupIdRequestDto: GroupIdRequestDto): MutableList<PublicTradeResponseDto> {
+        return groupAccountService.postTradeHistory(groupIdRequestDto)
+    }
+
+    override suspend fun postGroupMember(groupIdRequestDto: GroupIdRequestDto): MutableList<FriendResponseDto> {
+        return groupAccountService.postGroupMember(groupIdRequestDto)
+    }
+
+    override suspend fun postDuesHistory(groupIdRequestDto: GroupIdRequestDto): MutableList<DuesResponseDto> {
+        return groupAccountService.postDuesHistory(groupIdRequestDto)
+    }
+
+    override suspend fun postDuesHistoryDetail(groupDuesRequestDto: GroupDuesRequestDto): MutableList<DuesResponseDto> {
+        return groupAccountService.postDuesHistoryDetail(groupDuesRequestDto)
+    }
+
+    override suspend fun postPayDues(remitDuesResponseDto: DuesResponseDto) {
+        return groupAccountService.postPayDues(remitDuesResponseDto)
+    }
+
+    override suspend fun postRegistDues(createGroupAccountRequestDto: CreateGroupAccountRequestDto) {
+        return groupAccountService.postRegistDues(createGroupAccountRequestDto)
+    }
+
+    override suspend fun putDeleteDues(groupDuesRequestDto: GroupDuesRequestDto) {
+        return groupAccountService.putDeleteDues(groupDuesRequestDto)
+    }
 }
--- android/app/src/main/java/com/finance/android/viewmodels/GroupAccountViewModel.kt
+++ android/app/src/main/java/com/finance/android/viewmodels/GroupAccountViewModel.kt
@@ -2,10 +2,14 @@ package com.finance.android.viewmodels
 
 import android.app.Application
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.viewModelScope
+import com.finance.android.domain.dto.response.PublicAccountResponseDto
 import com.finance.android.domain.repository.BaseRepository
 import com.finance.android.domain.repository.GroupAccountRepository
 import com.finance.android.domain.repository.StockRepository
+import com.finance.android.utils.Response
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,4 +20,20 @@ class GroupAccountViewModel @Inject constructor(
 ) : BaseViewModel(application, baseRepository){
 
     val name = mutableStateOf("")
+
+    //모임 통장 조회
+    private val _groupAccountData = mutableStateOf<Response<MutableList<PublicAccountResponseDto>>>(Response.Loading)
+    val groupAccountData = _groupAccountData
+    fun getGroupAccountData() {
+        viewModelScope.launch {
+            this@GroupAccountViewModel.run {
+                groupAccountRepository.getGroupAccount()
+            }.collect{
+                _groupAccountData.value = it
+            }
+        }
+    }
+
+
+
 }
