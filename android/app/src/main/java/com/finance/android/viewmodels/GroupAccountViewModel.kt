aeb5ec5244522d6dd8a5b75f63f0c231e2908f11
letgodchan0 <letgodchan0@gmail.com>
2022-11-08 14:27:57 +0900

    feat: 모임 통장 구조 추가

--- android/app/src/main/java/com/finance/android/viewmodels/GroupAccountViewModel.kt
+++ android/app/src/main/java/com/finance/android/viewmodels/GroupAccountViewModel.kt
@@ -5,8 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import com.finance.android.domain.repository.BaseRepository
 import com.finance.android.domain.repository.GroupAccountRepository
 import com.finance.android.domain.repository.StockRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class GroupAccountViewModel @Inject constructor(
     application: Application,
     baseRepository: BaseRepository,
