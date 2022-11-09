d30b437eb57783612ae1ad75f6f43b293db3736d
이주영 <dlendjs93@gmail.com>
2022-11-09 17:20:22 +0900

    feat: 자산 관리 화면에 자산 추가 버튼 추가

--- android/app/src/main/java/com/finance/android/ui/components/HeaderAssetTabBar.kt
+++ android/app/src/main/java/com/finance/android/ui/components/HeaderAssetTabBar.kt
@@ -1,72 +1,97 @@
 package com.finance.android.ui.components
 
-
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Tab
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.airbnb.lottie.compose.*
 import com.finance.android.R
 import com.finance.android.ui.screens.AssetBankScreen
 import com.finance.android.ui.screens.AssetCardScreen
 import com.finance.android.ui.screens.AssetLifeScreen
 import com.finance.android.ui.screens.AssetStockScreen
 import com.finance.android.ui.theme.Disabled
-//import com.google.accompanist.pager.*
+import com.finance.android.utils.Const
+
+// import com.google.accompanist.pager.*
 
 @Composable
 fun HeaderAssetTabBar(
     modifier: Modifier,
     navController: NavController
 ) {
-
     var selectedIndex by remember { mutableStateOf(0) }
-
 
     val list = listOf("은행", "카드", "라이프", "증권")
     Column(modifier = modifier.fillMaxSize()) {
-        TabRow(
-
-            selectedTabIndex = selectedIndex,
-            backgroundColor = androidx.compose.material3.MaterialTheme.colorScheme.background,
-            modifier = Modifier
-                .padding(end = 0.dp),
-            indicator = {
-                TabRowDefaults.Indicator(
-                    modifier = Modifier
-                        .tabIndicatorOffset(it[selectedIndex]),
-                    color = Color.Transparent,
-                    height = TabRowDefaults.IndicatorHeight * 1.5F
-                )
-            },
-            divider = {
-                TabRowDefaults.Divider(
-                    color = Color.Transparent
-                )
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = dimensionResource(id = R.dimen.padding_medium).value.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            try {
+                Class
+                    .forName("androidx.compose.material3.TabRowKt")
+                    .getDeclaredField("ScrollableTabRowMinimumTabWidth")
+                    .apply {
+                        isAccessible = true
+                    }
+                    .set(this, 0f)
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
 
-        ) {
-            list.forEachIndexed { index, text ->
-                val selected = selectedIndex == index
-                Tab(
-
-                    selected = selected,
-                    onClick = { selectedIndex = index },
-                    text = { Text(text = text, fontSize = 18.sp, fontWeight = FontWeight.Bold ) },
-                    modifier = Modifier.width(20.dp),
-                    selectedContentColor = Color.Black,
-                    unselectedContentColor = Disabled,
-
+            ScrollableTabRow(
+                modifier = Modifier.weight(1.0f),
+                selectedTabIndex = selectedIndex,
+                containerColor = MaterialTheme.colorScheme.background,
+                indicator = {},
+                divider = {},
+                edgePadding = 0.dp
+            ) {
+                list.forEachIndexed { index, text ->
+                    val selected = selectedIndex == index
+                    Tab(
+                        modifier = Modifier
+                            .padding(all = 5.dp)
+                            .clip(RoundedCornerShape(10.dp)),
+                        selected = selected,
+                        onClick = { selectedIndex = index },
+                        text = {
+                            Text(
+                                text = text,
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        },
+                        selectedContentColor = MaterialTheme.colorScheme.onSurface,
+                        unselectedContentColor = Disabled
                     )
+                }
+            }
+            Spacer(modifier = modifier.width(10.dp))
+            IconButton(
+                onClick = { navController.navigate(Const.Routes.ADD_ASSET) }
+            ) {
+                Icon(
+                    Icons.Default.Add,
+                    contentDescription = "add"
+                )
             }
         }
 
@@ -85,7 +110,4 @@ fun HeaderAssetTabBar(
             }
         }
     }
-
-
-
 }
