da673cdb9b020b0902c5f6027b120a2c5fa50411
채윤선 <yoonsun0809@gmail.com>
2022-11-09 19:05:42 +0900

    ﻿feat :친구 리스트 추가

--- android/app/src/main/java/com/finance/android/viewmodels/GroupAccountViewModel.kt
+++ android/app/src/main/java/com/finance/android/viewmodels/GroupAccountViewModel.kt
@@ -1,16 +1,14 @@
 package com.finance.android.viewmodels
 
 import android.app.Application
+
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.lifecycle.viewModelScope
-import com.finance.android.domain.dto.request.CreateGroupAccountRequestDto
-import com.finance.android.domain.dto.request.MemberRequestDto
 import com.finance.android.domain.dto.response.PublicAccountResponseDto
 import com.finance.android.domain.repository.BaseRepository
 import com.finance.android.domain.repository.GroupAccountRepository
-import com.finance.android.domain.repository.StockRepository
 import com.finance.android.utils.Response
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -21,7 +19,7 @@ class GroupAccountViewModel @Inject constructor(
     application: Application,
     baseRepository: BaseRepository,
     private val groupAccountRepository: GroupAccountRepository
-) : BaseViewModel(application, baseRepository){
+) : BaseViewModel(application, baseRepository) {
 
     val name = mutableStateOf("")
 
@@ -44,25 +42,38 @@ class GroupAccountViewModel @Inject constructor(
 
 
     //모임 통장 조회
-    private val _groupAccountData = mutableStateOf<Response<MutableList<PublicAccountResponseDto>>>(Response.Loading)
+    private val _groupAccountData =
+        mutableStateOf<Response<MutableList<PublicAccountResponseDto>>>(Response.Loading)
     val groupAccountData = _groupAccountData
     fun getGroupAccountData() {
         viewModelScope.launch {
             this@GroupAccountViewModel.run {
                 groupAccountRepository.getGroupAccount()
-            }.collect{
+            }.collect {
                 _groupAccountData.value = it
             }
         }
     }
 
     //모임통장생성시 친구리스트 만들기
-    var isSelect = mutableStateOf(false)
+    var selectFriendsList: Array<MutableState<Boolean>>? = null
 
-    fun selectFriend () {
-        isSelect.value = !isSelect.value
+    fun initSelectedFriendsList(size: Int) {
+        if (selectFriendsList != null) {
+            return
+        }
+        selectFriendsList = Array(size) { mutableStateOf(false) }
     }
 
+    fun onClickFriend(index: Int) {
+        if (selectFriendsList == null) {
+            return
+        }
+        selectFriendsList!![index].value = !selectFriendsList!![index].value
+        println(selectFriendsList!![index].value)
+        Log.i("group", "${selectFriendsList!![index].value}")
+        Log.i("group", "$index")
+    }
 
 
 }
