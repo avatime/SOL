39b1e4eca8522ffa29bb99b4f3c1d3607412fc3e
민경욱 <rrkcl7733@naver.com>
2022-11-10 19:44:05 +0900

    feat: 보험 상세 구현

--- android/app/src/main/java/com/finance/android/ui/fragments/InsuranceDetailFragment.kt
+++ android/app/src/main/java/com/finance/android/ui/fragments/InsuranceDetailFragment.kt
@@ -1,34 +1,51 @@
 package com.finance.android.ui.fragments
 
-import androidx.compose.foundation.background
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.finance.android.R
+import com.finance.android.domain.dto.response.InsuranceDetailResponseDto
 import com.finance.android.ui.components.BackHeaderBar
+import com.finance.android.utils.Response
+import com.finance.android.viewmodels.InsuranceDetailViewModel
 
 @Composable
 fun InsuranceDetailFragment(
+    insuranceDetailViewModel: InsuranceDetailViewModel = hiltViewModel(),
+    isId: Int,
     name: String,
     onClose: () -> Unit
 ) {
+    LaunchedEffect(Unit) {
+        insuranceDetailViewModel.load(isId)
+    }
     Scaffold(
         topBar = {
-            BackHeaderBar(text = "보험입니다", modifier = Modifier, onClickBack = onClose)
+            BackHeaderBar(text = "보험", modifier = Modifier, onClickBack = onClose)
         }
     ) { innerPaddingModifier ->
         Column(
@@ -51,7 +68,7 @@ fun InsuranceDetailFragment(
                         .data("https://gamulbucket2022.s3.ap-northeast-2.amazonaws.com/finance/shinhanLife.png")
                         .crossfade(true)
                         .build(),
-                    contentDescription = "기업로고",
+                    contentDescription = null,
                     contentScale = ContentScale.Crop
                 )
                 Text(
@@ -88,6 +105,91 @@ fun InsuranceDetailFragment(
                     )
                 }
             }
+            when (val data = insuranceDetailViewModel.getLoadState()) {
+                is Response.Success -> {
+                    ContentList(
+                        accData = (insuranceDetailViewModel.insuranceDetail.value as Response.Success).data
+                    )
+                }
+                is Response.Loading -> {}
+                else -> {}
+            }
+        }
+    }
+}
+
+@Composable
+fun ContentList(
+    accData: InsuranceDetailResponseDto
+) {
+    Column(
+        modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_medium) / 2)
+    ) {
+        accData.detail.forEach {
+            val text = it.replace("\n\n", "\n").split("\n")
+            val expanded = remember { mutableStateOf(false) }
+            val expandable = 1 < text.size
+
+            Column(
+                modifier =
+                if (expandable) Modifier
+                    .padding(horizontal = 15.dp / 2)
+                    .clip(RoundedCornerShape(10.dp))
+                    .clickable { expanded.value = !expanded.value }
+                    .padding(
+                        vertical = dimensionResource(R.dimen.padding_medium) / 2,
+                        horizontal = 15.dp / 2
+                    ).animateContentSize()
+                else Modifier
+                    .padding(
+                        vertical = dimensionResource(R.dimen.padding_medium) / 2,
+                        horizontal = 15.dp
+                    ).animateContentSize()
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 15.dp / 2),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .weight(1f),
+                        text = text[0],
+                        fontWeight = FontWeight.Bold
+                    )
+                    Spacer(modifier = Modifier.width(15.dp))
+                    if (expandable) {
+                        Image(
+                            modifier = Modifier
+                                .size(24.dp),
+                            painter = painterResource(id = if (expanded.value) R.drawable.down else R.drawable.up),
+                            contentDescription = null
+                        )
+                    }
+                }
+                if (expanded.value && expandable) {
+                    Divider(
+                        modifier = Modifier.padding(
+                            vertical = dimensionResource(R.dimen.padding_medium) / 2,
+                            horizontal = 15.dp
+                        ),
+                        color = Color(0xFFE5D8FD)
+                    )
+                }
+                if (expanded.value && expandable) {
+                    Text(
+                        modifier = Modifier.padding(horizontal = 15.dp),
+                        text = text.slice(1 until text.size).joinToString("\n")
+                    )
+                }
+            }
+            Divider(
+                modifier = Modifier.padding(
+                    vertical = dimensionResource(R.dimen.padding_medium) / 2,
+                    horizontal = 15.dp
+                )
+            )
         }
     }
 }
@@ -99,7 +201,7 @@ fun Item(
     title: String,
     detail: String
 ) {
-    Column (
+    Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
