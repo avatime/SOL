b6dd8f4953b197c3d7c99d729e7a8605d00ea4df
채윤선 <yoonsun0809@gmail.com>
2022-11-11 16:21:22 +0900

    ﻿fix : 오류 수정

--- android/app/src/main/java/com/finance/android/ui/screens/groupAccount/GroupAccountMemberScreen.kt
+++ android/app/src/main/java/com/finance/android/ui/screens/groupAccount/GroupAccountMemberScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -29,7 +31,11 @@ fun GroupAccountMemberScreen(
     LaunchedEffect(Unit) {
         launch()
     }
-    Column(modifier = modifier.background(Color.White)) {
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize()
+    ) {
         when (val response = groupAccountViewModel.groupAccountMemberData.value) {
             is Response.Failure -> Text(text = "실패")
             is Response.Loading -> AnimatedLoading(text = "가져오고 있어요")
--- android/app/src/main/java/com/finance/android/ui/screens/groupAccount/GroupAccountTradeDetailScreen.kt
+++ android/app/src/main/java/com/finance/android/ui/screens/groupAccount/GroupAccountTradeDetailScreen.kt
@@ -1,16 +1,21 @@
 package com.finance.android.ui.screens.groupAccount
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import com.finance.android.ui.components.AnimatedLoading
 import com.finance.android.ui.components.showHistoryList
+import com.finance.android.ui.components.ButtonType
+import com.finance.android.ui.components.TextButton
+import com.finance.android.utils.Const
 import com.finance.android.utils.Response
 import com.finance.android.viewmodels.GroupAccountViewModel
 
@@ -20,23 +25,56 @@ fun GroupAccountTradeDetailScreen(
     groupAccountViewModel: GroupAccountViewModel,
     modifier: Modifier = Modifier,
 ) {
-    fun launch(){
+
+    fun launch() {
         groupAccountViewModel.getDuesTradeHistory(groupAccountViewModel.paId.value)
     }
 
-    LaunchedEffect(Unit){
+    LaunchedEffect(Unit) {
         launch()
     }
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
 
-    Column(modifier = modifier.background(Color.White)) {
-        when (val response = groupAccountViewModel.duesTradeHistoryData.value){
+
+        when (val response = groupAccountViewModel.duesTradeHistoryData.value) {
+
             is Response.Failure -> Text(text = "실패")
             is Response.Loading -> AnimatedLoading(text = "조그만 기다려주세요!")
             is Response.Success -> {
-                showHistoryList(modifier = Modifier.weight(1.0f),
-                    historyList = List(response.data.size) {i -> response.data[i].toEntity()})
-            }
-        }
-    }
+                Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                    showHistoryList(modifier = Modifier.weight(1.0f),
 
+                        historyList = List(response.data.size) { i -> response.data[i].toEntity() })
+                }
+
+
+            }
+
+        }//end of when
+
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.weight(1f)
+        ) {
+            Spacer(modifier = Modifier.weight(0.3f))
+            TextButton(
+                onClick = { navController.navigate(Const.GROUP_ACCOUNT_INPUT_MONEY_SCREEN) },
+                text = "입금",
+                buttonType = ButtonType.ROUNDED
+            )
+            Spacer(modifier = Modifier.weight(0.3f))
+            TextButton(
+                onClick = { navController.navigate(Const.GROUP_ACCOUNT_INPUT_MONEY_SCREEN) },
+                text = "출금",
+                buttonType = ButtonType.ROUNDED
+            )
+            Spacer(modifier = Modifier.weight(0.3f))
+        }//end of Row
+    }//end of column
 }
