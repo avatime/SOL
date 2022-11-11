5806681d83c6c86b43b638d46e6a852fd191e712
이주영 <dlendjs93@gmail.com>
2022-11-11 16:30:17 +0900

    feat: 은행선택 다이얼로그 완성

--- frontend2/src/pages/SendPage.tsx
+++ frontend2/src/pages/SendPage.tsx
@@ -16,6 +16,7 @@ import { TransitionProps } from "@mui/material/transitions";
 import React from "react";
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import BankInfoRes from "../apis/response/BankInfoRes";
 import SelectBankBottomDialog from "../components/SelectBankBottomDialog";
 
 function SendPage() {
@@ -31,21 +32,22 @@ function SendPage() {
   //
 
   const [openBottomDialog, setOpenBottomDialog] = useState(false);
+  const [bankInfo, setBankInfo] = useState<BankInfoRes>();
 
   return (
     <Box p={2} display="flex" flexDirection="column" height="100vh">
       <Stack direction="row" alignItems="center">
         <img src="/favicon.ico" width="24px" height="24px" />
-        <Typography mx={1} variant="h6" component="h6" fontWeight="bold">
+        <Typography mx={1} variant="subtitle2" fontWeight="bold">
           SOL#
         </Typography>
       </Stack>
-      <Typography mt={2} variant="subtitle1" component="h6" color="primary">
+      <Typography mt={2} variant="body2" color="primary">
         {senderName}님이
         <br />
         {value.toLocaleString()}원을 보냈어요.
       </Typography>
-      <Typography mt={2} variant="h4" component="h4" fontWeight="bold">
+      <Typography mt={2} variant="h6" fontWeight="bold">
         어디로 받을까요?
       </Typography>
       <Box flex="1" />
@@ -55,6 +57,10 @@ function SendPage() {
       <SelectBankBottomDialog
         open={openBottomDialog}
         onClose={() => setOpenBottomDialog(false)}
+        onClickBankItem={(it) => {
+          setBankInfo(it);
+          setOpenBottomDialog(false);
+        }}
       />
     </Box>
   );
