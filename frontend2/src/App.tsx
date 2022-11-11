162f16fcfb340e40b37e0ef08154faa166c1f38f
이주영 <dlendjs93@gmail.com>
2022-11-11 14:12:30 +0900

    feat: 비회원 송금 화면 개발 중

---
+++ frontend2/src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+import { Route } from "react-router";
+import SendPage from "./pages/SendPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#0046FF",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/send" element={<SendPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
