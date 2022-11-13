f90af3a30610459e7a033fd1b05af648fd3a44b3
이주영 <dlendjs93@gmail.com>
2022-11-14 08:28:03 +0900

    ﻿feat: 연락처로 송금 시, 문자메시지 띄우는 기능 추가

--- android/app/src/main/java/com/finance/android/ui/screens/remit/ContactListScreen.kt
+++ android/app/src/main/java/com/finance/android/ui/screens/remit/ContactListScreen.kt
@@ -3,23 +3,35 @@ package com.finance.android.ui.screens.remit
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.util.Log
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
+import com.finance.android.domain.dto.response.ContactDto
 import com.finance.android.ui.components.ContactItem
 import com.finance.android.utils.retrieveAllContacts
 import com.finance.android.viewmodels.RemitViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 
 @Composable
-fun ContactListScreen(remitViewModel: RemitViewModel, navController: NavController) {
+fun ContactListScreen(
+    modifier: Modifier = Modifier.background(Color.White),
+    remitViewModel: RemitViewModel,
+    navController: NavController
+) {
     remitViewModel.requestRemit.value = true
     if (ActivityCompat.checkSelfPermission(
             LocalContext.current,
@@ -30,22 +42,31 @@ fun ContactListScreen(remitViewModel: RemitViewModel, navController: NavControll
     }
 
 
-    val list = LocalContext.current.retrieveAllContacts()
-    Log.i("TEST", list.toString())
-    Box(Modifier.fillMaxSize()) {
+    val context = LocalContext.current
+    val list = remember { mutableStateOf<List<ContactDto>>(emptyList()) }
+    LaunchedEffect(Unit) {
+        withContext(Dispatchers.IO) {
+            list.value = context.retrieveAllContacts().filter { it.phoneNumber.isNotEmpty() }
+        }
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+    ) {
 
-        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-            Spacer(modifier = Modifier.padding(10.dp))
-
-            list.forEach{
-                 ContactItem(
-                     name = it.name,
-                     number = it.phoneNumber[0],
-                     avatar = it.avatar,
-                     remitViewModel = remitViewModel,
-                     navController= navController
-                 ) }
-
+        LazyColumn {
+            items(count = list.value.size) { idx ->
+                val it = list.value[idx]
+                ContactItem(
+                    name = it.name,
+                    number = it.phoneNumber[0],
+                    avatar = it.avatar,
+                    modifier = Modifier,
+                    remitViewModel = remitViewModel,
+                    navController = navController
+                )
+            }
         }
 
 
