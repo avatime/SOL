01f316d7a8a1b1bb2b611ebac77ea767512cb196
이지나 <dlwlsk0428@naver.com>
2022-11-14 10:38:57 +0900

    feat : 주식 미니바

--- android/app/src/main/java/com/finance/android/viewmodels/HomeViewModel.kt
+++ android/app/src/main/java/com/finance/android/viewmodels/HomeViewModel.kt
@@ -8,8 +8,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.viewModelScope
 import com.finance.android.datastore.WalkStore
 import com.finance.android.domain.dto.response.AccountRegisteredResponseDto
+import com.finance.android.domain.dto.response.FinanceResponseDto
 import com.finance.android.domain.repository.BankRepository
 import com.finance.android.domain.repository.BaseRepository
+import com.finance.android.domain.repository.StockRepository
 import com.finance.android.utils.Response
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -20,14 +22,17 @@ class HomeViewModel @Inject constructor(
     application: Application,
     baseRepository: BaseRepository,
     private val bankRepository: BankRepository,
+    private val stockRepository: StockRepository
 ) : BaseViewModel(application, baseRepository), SensorEventListener {
     val mainData = mutableStateOf<Response<AccountRegisteredResponseDto>>(Response.Loading)
+    val stockList = mutableStateOf<Array<FinanceResponseDto>>(arrayOf())
     val walkCount = mutableStateOf<Int?>(null)
 
     fun load() {
         viewModelScope.launch {
             loadList()
             loadWalkCount()
+            loadStockList()
         }
     }
 
@@ -62,6 +67,17 @@ class HomeViewModel @Inject constructor(
         viewModelScope.launch {
             loadWalkCount()
         }
+    }
+
+    private suspend fun loadStockList() {
+        this@HomeViewModel.run {
+            stockRepository.getFinanceList()
+        }
+            .collect {
+                if (it is Response.Success) {
+                    stockList.value = it.data
+                }
+            }
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
