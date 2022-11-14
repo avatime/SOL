ec9ce538378724230db28f4d69ea2bd3ff7cd025
이주영 <dlendjs93@gmail.com>
2022-11-14 15:27:12 +0900

    feat: 송금 시, 연락처 화면 페이징 처리 추가

--- android/app/src/main/java/com/finance/android/domain/dto/response/ContactDto.kt
+++ android/app/src/main/java/com/finance/android/domain/dto/response/ContactDto.kt
@@ -1,14 +1,8 @@
 package com.finance.android.domain.dto.response
 
-import com.google.gson.annotations.SerializedName
-
 data class ContactDto(
-    @SerializedName("contact_id")
     val contactId: Long,
-    @SerializedName("name")
     val name: String,
-    @SerializedName("phone_number")
-    val phoneNumber: List<String>,
-    @SerializedName("avatar")
+    val phoneNumber: String,
     val avatar: String
 )
