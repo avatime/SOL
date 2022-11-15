c04cbc5137c1112d7db498625d3fbd39608183f8
이지나 <dlwlsk0428@naver.com>
2022-11-15 09:25:42 +0900

    feat : qr 생성

--- android/app/src/main/java/com/finance/android/ui/fragments/QrCodeFragment.kt
+++ android/app/src/main/java/com/finance/android/ui/fragments/QrCodeFragment.kt
@@ -1,5 +1,8 @@
 package com.finance.android.ui.fragments
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -10,15 +13,18 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.finance.android.R
 import com.finance.android.ui.components.BackHeaderBar
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.qrcode.QRCodeWriter
-import com.journeyapps.barcodescanner.BarcodeEncoder
+import com.journeyapps.barcodescanner.*
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -88,11 +94,48 @@ private fun QRScanner(
                 .background(color = MaterialTheme.colorScheme.background)
                 .padding(dimensionResource(R.dimen.padding_medium))
         ) {
+            AdminClubMembershipScanScreen()
             Text(text = "QR 스캐너", fontSize = 20.sp, fontWeight = FontWeight.ExtraBold)
         }
     }
 }
 
+@Composable
+fun AdminClubMembershipScanScreen() {
+    val context = LocalContext.current
+    var scanFlag by remember {
+        mutableStateOf(false)
+    }
+
+    val compoundBarcodeView = remember {
+        CompoundBarcodeView(context).apply {
+            val capture = CaptureManager(context as Activity, this)
+            capture.initializeFromIntent(context.intent, null)
+            this.setStatusText("")
+            capture.decode()
+            this.decodeContinuous { result ->
+                if(scanFlag){
+                    return@decodeContinuous
+                }
+                scanFlag = true
+                result.text?.let { qrCode->
+                    //Do something and when you finish this something
+                    println(qrCode)
+                    //put scanFlag = false to scan another item
+                    scanFlag = false
+                }
+                //If you don't put this scanFlag = false, it will never work again.
+                //you can put a delay over 2 seconds and then scanFlag = false to prevent multiple scanning
+
+            }
+        }
+    }
+
+    AndroidView(
+        modifier = Modifier,
+        factory = { compoundBarcodeView },
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
@@ -115,7 +158,7 @@ private fun MyQRCode(
                 .padding(top = it.calculateTopPadding())
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
-                .background(color = MaterialTheme.colorScheme.background)
+                .background(color = MaterialTheme.colorScheme.surface)
                 .padding(dimensionResource(R.dimen.padding_medium))
         ) {
             val barcodeEncoder = BarcodeEncoder()
@@ -123,11 +166,36 @@ private fun MyQRCode(
             val bitmap = barcodeEncoder.encodeBitmap(
                 "신한,11111111111",
                 BarcodeFormat.QR_CODE,
-                400,
-                400
+                1200,
+                1200
             ).asImageBitmap()
 
-            Image(bitmap = bitmap, contentDescription = "qr 코드")
+            ForceBrightness()
+            Image(modifier = Modifier.aspectRatio(1f).fillMaxWidth(), bitmap = bitmap, contentDescription = "qr 코드")
         }
     }
+}
+
+@Composable
+fun ForceBrightness(brightness: Float = 1f) {
+    val activity = requireNotNull(LocalContext.current.getActivity())
+    DisposableEffect(Unit) {
+        val attributes = activity.window.attributes
+        val originalBrightness = attributes.screenBrightness
+        activity.window.attributes = attributes.apply { screenBrightness = brightness }
+        onDispose {
+            activity.window.attributes = attributes.apply { screenBrightness = originalBrightness }
+        }
+    }
+}
+
+fun Context.getActivity(): Activity? {
+    var context = this
+    while (context is ContextWrapper) {
+        if (context is Activity) {
+            return context
+        }
+        context = context.baseContext
+    }
+    return null
 }
