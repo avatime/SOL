778a3e119d81a281599261f662d6be259f5aef02
이주영 <dlendjs93@gmail.com>
2022-11-15 10:23:08 +0900

    refactor: 로딩 로직 리팩토링 - 상품 화면

--- android/app/src/main/java/com/finance/android/ui/components/HeaderProductTabBar.kt
+++ android/app/src/main/java/com/finance/android/ui/components/HeaderProductTabBar.kt
@@ -7,7 +7,6 @@ import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
@@ -20,71 +19,82 @@ import com.finance.android.domain.dto.response.CardRecommendResponseDto
 import com.finance.android.ui.screens.CardProductCheckScreen
 import com.finance.android.ui.screens.CardProductCreditScreen
 import com.finance.android.ui.theme.Disabled
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun HeaderProductTabBar(
     modifier: Modifier,
     navController: NavController,
     cardRecommendList: CardRecommendResponseDto
 ) {
-    var selectedIndex by remember { mutableStateOf(0) }
+    val pagerState = rememberPagerState()
+    val coroutineScope = rememberCoroutineScope()
 
     val list = listOf("신용카드", "체크카드")
     Column(modifier = modifier.fillMaxSize()) {
-        Row(
-            modifier = modifier
-                .padding(horizontal = dimensionResource(id = R.dimen.padding_medium).value.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            try {
-                Class
-                    .forName("androidx.compose.material3.TabRowKt")
-                    .getDeclaredField("ScrollableTabRowMinimumTabWidth")
-                    .apply {
-                        isAccessible = true
-                    }
-                    .set(this, 0f)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-
-            ScrollableTabRow(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                selectedTabIndex = selectedIndex,
-                containerColor = MaterialTheme.colorScheme.background,
-                indicator = {},
-                divider = {},
-                edgePadding = 0.dp
-            ) {
-                list.forEachIndexed { index, text ->
-                    val selected = selectedIndex == index
-                    Tab(
-                        modifier = Modifier
-                            .padding(5.dp)
-                            .clip(RoundedCornerShape(10.dp)),
-                        selected = selected,
-                        onClick = { selectedIndex = index },
-                        text = {
-                            Text(
-                                text = text,
-                                fontSize = 18.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                        },
-                        selectedContentColor = MaterialTheme.colorScheme.onSurface,
-                        unselectedContentColor = Disabled
-                    )
+        try {
+            Class
+                .forName("androidx.compose.material3.TabRowKt")
+                .getDeclaredField("ScrollableTabRowMinimumTabWidth")
+                .apply {
+                    isAccessible = true
                 }
+                .set(this, 0f)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        ScrollableTabRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = dimensionResource(id = R.dimen.padding_medium).value.dp),
+            selectedTabIndex = pagerState.currentPage,
+            containerColor = MaterialTheme.colorScheme.background,
+            indicator = {},
+            divider = {},
+            edgePadding = 0.dp
+        ) {
+            list.forEachIndexed { index, text ->
+                Tab(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .clip(RoundedCornerShape(10.dp)),
+                    selected = pagerState.currentPage == index,
+                    onClick = { coroutineScope.launch { pagerState.animateScrollToPage(index) } },
+                    text = {
+                        Text(
+                            text = text,
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    },
+                    selectedContentColor = MaterialTheme.colorScheme.onSurface,
+                    unselectedContentColor = Disabled
+                )
             }
         }
 
-        when (selectedIndex) {
-            0 -> {
-                CardProductCreditScreen(navController = navController, creditCardList = cardRecommendList.creditCardList)
-            }
-            1 -> {
-                CardProductCheckScreen(navController = navController, checkCardList = cardRecommendList.checkCardList)
+        HorizontalPager(
+            count = list.size,
+            state = pagerState
+        ) {
+            when (it) {
+                0 -> {
+                    CardProductCreditScreen(
+                        navController = navController,
+                        creditCardList = cardRecommendList.creditCardList
+                    )
+                }
+                1 -> {
+                    CardProductCheckScreen(
+                        navController = navController,
+                        checkCardList = cardRecommendList.checkCardList
+                    )
+                }
             }
         }
     }
