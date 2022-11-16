b7f7d57ab449e762fa11a867cb36d900f7d07f84
채윤선 <yoonsun0809@gmail.com>
2022-11-16 09:48:41 +0900

    ﻿feat 모임통장 비회원 초대기능추가

--- android/app/src/main/java/com/finance/android/ui/screens/groupAccount/GroupAccountMemberScreen.kt
+++ android/app/src/main/java/com/finance/android/ui/screens/groupAccount/GroupAccountMemberScreen.kt
@@ -1,5 +1,9 @@
 package com.finance.android.ui.screens.groupAccount
 
+import android.app.Application
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,16 +13,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import com.finance.android.ui.components.AnimatedLoading
 import com.finance.android.ui.components.GroupAccountMemberListItem
+import com.finance.android.utils.Const
 import com.finance.android.utils.Response
 import com.finance.android.viewmodels.GroupAccountViewModel
+import dagger.hilt.android.internal.Contexts.getApplication
+import java.text.DecimalFormat
+import java.util.*
 
 @Composable
 fun GroupAccountMemberScreen(
     groupAccountViewModel: GroupAccountViewModel
 ) {
-
+    val context = LocalContext.current
     fun launch() {
         groupAccountViewModel.getGroupAccountMember(groupAccountViewModel.paId.value)
     }
@@ -41,12 +50,33 @@ fun GroupAccountMemberScreen(
                         GroupAccountMemberListItem(
                             img = item.pfImg,
                             name = item.userName,
-                            type = item.type
+                            type = item.type,
+                            phoneNumber = item.phone,
+                            onClickNonMember = {
+                                sendInviteMessage(context= context, phoneNumber = item.phone)
+
+                            }
                         )
                     })
                 }
             }
         }
     }
+
+}
+fun sendInviteMessage(context: Context, phoneNumber: String) {
+    val link =
+        "${Const.WEB_API}"
+
+    val smsUri = Uri.parse("sms:${phoneNumber.replace("-", "")}")
+    val sendIntent = Intent(Intent.ACTION_SENDTO, smsUri).apply {
+        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        putExtra(
+            "sms_body",
+            "아래 링크로 접속해서 앱을 다운로드 받은 후 모두의 통장을 사용해보세요!.\n$link"
+        )
+    }
+    context.startActivity(sendIntent)
+
 
 }
