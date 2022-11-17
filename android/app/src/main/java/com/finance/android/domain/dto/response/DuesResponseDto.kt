b62f43cc0722b0095296a9e4eb3ea9b91d86e6a2
이주영 <dlendjs93@gmail.com>
2022-11-17 17:57:21 +0900

    feat: 모두의 통장 회비 내역에서 대상자 보는 기능 추가

--- android/app/src/main/java/com/finance/android/domain/dto/response/DuesResponseDto.kt
+++ android/app/src/main/java/com/finance/android/domain/dto/response/DuesResponseDto.kt
@@ -1,26 +1,32 @@
 package com.finance.android.domain.dto.response
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-import java.time.LocalDate
 import java.time.LocalDateTime
 
 data class DuesResponseDto(
     @SerializedName("paid")
-    val paid : Boolean,//유저 지불 여부
+    val paid: Boolean,
+    // 유저 지불 여부
     @SerializedName("dues_id")
-    val duesId : Int, //회비 아이디
+    val duesId: Int, // 회비 아이디
     @SerializedName("dues_name")
-    val duesName : String, //회비 이름
+    val duesName: String, // 회비 이름
     @SerializedName("created_at")
-    val createdAt : String,//생성 날짜
+    val createdAt: String,
+    // 생성 날짜
     @SerializedName("due_date")
-    val dueDate : LocalDateTime, //만기일 (정기납부일때만)
+    val dueDate: LocalDateTime, // 만기일 (정기납부일때만)
     @SerializedName("dues_val")
-    val duesVal : Int, //금액
+    val duesVal: Int, // 금액
     @SerializedName("paid_user")
-    val paidUser : Int,//회비 낸 인원수
+    val paidUser: Int,
+    // 회비 낸 인원수
     @SerializedName("total_user")
-    val totalUSer : Int, //전체 인원 수
+    val totalUSer: Int, // 전체 인원 수
     @SerializedName("creator")
-    val creator : String //만든 사람
+    val creator: String, // 만든 사람
+    @SerializedName("dues_detail_res")
+    @Expose
+    val duesDetailResponseDto: DuesDetailResponseDto
 )
--- android/app/src/main/java/com/finance/android/domain/dto/response/FriendResponseDto.kt
+++ android/app/src/main/java/com/finance/android/domain/dto/response/FriendResponseDto.kt
@@ -1,17 +1,23 @@
 package com.finance.android.domain.dto.response
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class FriendResponseDto(
     var id: Int? = null,
     @SerializedName("user_name")
+    @Expose
     val userName : String, //친구 이름
     @SerializedName("type")
+    @Expose
     val type : String, //유저타입(회원, 비회원)
     @SerializedName("pf_img")
+    @Expose
     val pfImg : String,//프로필이미지
     @SerializedName("pf_name")
+    @Expose
     val pfName : String, //프로필 이름
     @SerializedName("phone")
+    @Expose
     val phone : String // 폰 번호
 )
