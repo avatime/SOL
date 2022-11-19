e84810ad11777f2592a78348e8a86f18f1f0a831
이주영 <dlendjs93@gmail.com>
2022-11-20 00:07:02 +0900

    fix: 누락된 부분 추가

---
+++ frontend2/src/components/Loading.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+export default function Loading() {
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="100vh">
+      <img src="/favicon.ico" />
+    </Box>
+  );
+}
