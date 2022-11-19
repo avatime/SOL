b05ec72434fe6d901acd13a74e2d3a4ea1c2684c
이주영 <dlendjs93@gmail.com>
2022-11-20 00:07:02 +0900

    fix: 누락된 부분 추가

--- frontend2/src/pages/ExpirationTokenPage.tsx
+++ frontend2/src/pages/ExpirationTokenPage.tsx
@@ -1,17 +1,16 @@
-import { Typography } from "@mui/material";
-import Box from "@mui/material/Box";
+import { Typography, Stack, Box} from "@mui/material";
 import EmojiIcon from "@mui/icons-material/SentimentDissatisfied";
 
 function ExpirationTokenPage() {
   return (
-    <Box display="flex" flexDirection="column" alignItems="center">
-      <Box mt={10}>
+    <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" height="100vh">
+      <Stack direction="column">
         <EmojiIcon style={{"fontSize": "200px"}}/>
-      </Box>
 
       <Typography variant="h5" component="h5">
         토큰이 만료됐습니다
       </Typography>
+    </Stack>
     </Box>
   );
 }
