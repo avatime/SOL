98be972e6ccb1d50c6439354ac36740e8099e22e
이주영 <dlendjs93@gmail.com>
2022-11-23 20:07:34 +0900

    feat: 송금 시, 간편 비밀번호 입력 화면 추가

--- android/app/src/main/java/com/finance/android/ui/screens/remit/InputMoneyScreen.kt
+++ android/app/src/main/java/com/finance/android/ui/screens/remit/InputMoneyScreen.kt
@@ -1,9 +1,11 @@
 package com.finance.android.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -18,6 +20,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.finance.android.ui.components.AnimatedLoading
 import com.finance.android.ui.components.ButtonType
 import com.finance.android.utils.Const
 import com.finance.android.utils.NumberCommaTransformation
@@ -32,10 +35,11 @@ import java.util.regex.Pattern
 fun InputMoneyScreen(
     modifier: Modifier,
     remitViewModel: RemitViewModel,
-    navController: NavController
+    innerNavController: NavController,
+    outerNavController: NavController
 ) {
     var moneyValue by remember {
-        mutableStateOf("")
+        remitViewModel.moneyValue
     }
 
     var balance by remember {
@@ -74,159 +78,178 @@ fun InputMoneyScreen(
     LaunchedEffect(Unit) {
         focusRequester.requestFocus()
     }
-    Row(
-        modifier = modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Column() {
-            if (isNext) {
-                TextButton(
-                    onClick = { isNext = false },
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor = Color.Transparent,
-                        contentColor = Color.Black
-                    ),
-                    modifier = Modifier.padding(start = 20.dp)
 
-                ) {
-                    Text(
-                        text = DecimalFormat("#,###원").format(moneyValue.toInt()) + "을",
-                        fontSize = 30.sp,
-                        softWrap = false,
-                        maxLines = 1
-                    )
-                    Text(
-                        text = "보낼까요?",
-                        fontSize = 30.sp,
-                        softWrap = false,
-                        maxLines = 1
-                    )
-                }
-            } else {
-                TextField(
-                    value = moneyValue,
-                    onValueChange = {
-                        if (!Pattern.matches("^[0-9]*$", it)) return@TextField
-                        if (it.isNotEmpty() && it.toLong() > Int.MAX_VALUE) return@TextField
-                        if (error.value && moneyValue < it) {
-                            return@TextField
-                        } else if (moneyValue == it) return@TextField
-
-                        moneyValue = if (it.isEmpty()) "" else it.toInt().toString()
-                    },
-                    modifier = Modifier
-                        .padding(start = 16.dp)
-                        .fillMaxWidth()
-                        .focusRequester(focusRequester),
-                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                    singleLine = true,
-                    placeholder = {
-                        Text(
-                            text = placeholderText.value,
-                            fontSize = 30.sp,
-                            textAlign = TextAlign.Center
-                        )
-                    },
-                    colors = TextFieldDefaults
-                        .textFieldColors(
-                            backgroundColor = Transparent,
-                            focusedIndicatorColor = Transparent,
-                            unfocusedIndicatorColor = Transparent,
-                            cursorColor = Transparent
+    Box {
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column() {
+                if (isNext) {
+                    TextButton(
+                        onClick = { isNext = false },
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = Color.Transparent,
+                            contentColor = Color.Black
                         ),
-                    textStyle = androidx.compose.ui.text.TextStyle().copy(fontSize = 40.sp),
-                    isError = error.value,
-                    visualTransformation = if ( moneyValue.isNotEmpty()) NumberCommaTransformation() else VisualTransformation.None
-                )
+                        modifier = Modifier.padding(start = 20.dp)
 
-                if (error.value) {
-                    Text(
-                        text = "잔액을 초과한 금액은 입력할 수 없습니다.",
-                        color = MaterialTheme.colors.error,
-                        style = MaterialTheme.typography.caption,
-                        modifier = Modifier.padding(start = 30.dp)
-                    )
-                }
-            }
-
-            if (moneyValue.isNotEmpty() && (Integer.parseInt(moneyValue) > balance!!)) {
-                TextButton(
-                    onClick = { moneyValue = balance.toString() },
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor = Color.Transparent,
-                        contentColor = Color.Black
-                    ),
-                    modifier = Modifier.padding(start = 30.dp)
-
-                ) {
-                    Text(
-                        text = "잔액 " + DecimalFormat("#,###원").format(balance) + "(클릭시 입력)",
-                        fontSize = 20.sp
-                    )
-                }
-            }
-
-            if (moneyValue.isEmpty()) {
-                TextButton(
-                    onClick = { moneyValue = balance.toString() },
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor = Color.Transparent,
-                        contentColor = Color.Black
-                    ),
-                    modifier = Modifier.padding(start = 30.dp)
-
-                ) {
-                    Text(
-                        text = "잔액 " + DecimalFormat("#,###원").format(balance) + "(클릭시 입력)",
-                        fontSize = 20.sp
-                    )
-                }
-            }
-
-            if (moneyValue.isNotEmpty() && 0 < moneyValue.toInt()) {
-                if (!isNext) {
-                    Spacer(modifier = Modifier.weight(1.0f))
-                    com.finance.android.ui.components.TextButton(
-                        onClick = { isNext = true },
-                        text = "다음",
-                        modifier = Modifier.withBottomButton(),
-                        buttonType = ButtonType.ROUNDED,
-                        enabled = !error.value
-                    )
+                    ) {
+                        Text(
+                            text = DecimalFormat("#,###원").format(moneyValue.toInt()) + "을",
+                            fontSize = 30.sp,
+                            softWrap = false,
+                            maxLines = 1
+                        )
+                        Text(
+                            text = "보낼까요?",
+                            fontSize = 30.sp,
+                            softWrap = false,
+                            maxLines = 1
+                        )
+                    }
                 } else {
-                    Spacer(modifier = Modifier.weight(1.0f))
+                    TextField(
+                        value = moneyValue,
+                        onValueChange = {
+                            if (!Pattern.matches("^[0-9]*$", it)) return@TextField
+                            if (it.isNotEmpty() && it.toLong() > Int.MAX_VALUE) return@TextField
+                            if (error.value && moneyValue < it) {
+                                return@TextField
+                            } else if (moneyValue == it) return@TextField
 
-                    com.finance.android.ui.components.TextButton(
-                        onClick = {
-                            if (!remitViewModel.requestRemit.value && Integer.parseInt(moneyValue) > 0) {
-                                remitViewModel.remitFromAccount(
-                                    value = Integer.parseInt(moneyValue),
-                                    receive = "",
-                                    send = "",
-                                    onSuccess = {
-                                        navController.navigate("${Const.REMIT_OK_SCREEN}/$moneyValue/송금 완료")
-                                        remitViewModel.moneyValue.value = moneyValue
-                                    }
-                                )
-                            } else if (Integer.parseInt(moneyValue) > 0) {
-                                remitViewModel.remitFromPhone(
-                                    value = Integer.parseInt(moneyValue),
-                                    receive = "",
-                                    send = "",
-                                    onSuccess = {
-                                        navController.navigate("${Const.REMIT_OK_SCREEN}/$moneyValue/$it")
-                                        remitViewModel.moneyValue.value = moneyValue
-                                    }
-                                )
-                            }
+                            moneyValue = if (it.isEmpty()) "" else it.toInt().toString()
                         },
-                        text = "보내기",
-                        modifier = Modifier.withBottomButton(),
-                        buttonType = ButtonType.ROUNDED
-
+                        modifier = Modifier
+                            .padding(start = 16.dp)
+                            .fillMaxWidth()
+                            .focusRequester(focusRequester),
+                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                        singleLine = true,
+                        placeholder = {
+                            Text(
+                                text = placeholderText.value,
+                                fontSize = 30.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        },
+                        colors = TextFieldDefaults
+                            .textFieldColors(
+                                backgroundColor = Transparent,
+                                focusedIndicatorColor = Transparent,
+                                unfocusedIndicatorColor = Transparent,
+                                cursorColor = Transparent
+                            ),
+                        textStyle = androidx.compose.ui.text.TextStyle().copy(fontSize = 40.sp),
+                        isError = error.value,
+                        visualTransformation = if (moneyValue.isNotEmpty()) NumberCommaTransformation() else VisualTransformation.None
                     )
+
+                    if (error.value) {
+                        Text(
+                            text = "잔액을 초과한 금액은 입력할 수 없습니다.",
+                            color = MaterialTheme.colors.error,
+                            style = MaterialTheme.typography.caption,
+                            modifier = Modifier.padding(start = 30.dp)
+                        )
+                    }
+                }
+
+                if (moneyValue.isNotEmpty() && (Integer.parseInt(moneyValue) > balance!!)) {
+                    TextButton(
+                        onClick = { moneyValue = balance.toString() },
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = Color.Transparent,
+                            contentColor = Color.Black
+                        ),
+                        modifier = Modifier.padding(start = 30.dp)
+
+                    ) {
+                        Text(
+                            text = "잔액 " + DecimalFormat("#,###원").format(balance) + "(클릭시 입력)",
+                            fontSize = 20.sp
+                        )
+                    }
+                }
+
+                if (moneyValue.isEmpty()) {
+                    TextButton(
+                        onClick = { moneyValue = balance.toString() },
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = Color.Transparent,
+                            contentColor = Color.Black
+                        ),
+                        modifier = Modifier.padding(start = 30.dp)
+
+                    ) {
+                        Text(
+                            text = "잔액 " + DecimalFormat("#,###원").format(balance) + "(클릭시 입력)",
+                            fontSize = 20.sp
+                        )
+                    }
+                }
+
+                if (moneyValue.isNotEmpty() && 0 < moneyValue.toInt()) {
+                    if (!isNext) {
+                        Spacer(modifier = Modifier.weight(1.0f))
+                        com.finance.android.ui.components.TextButton(
+                            onClick = { isNext = true },
+                            text = "다음",
+                            modifier = Modifier.withBottomButton(),
+                            buttonType = ButtonType.ROUNDED,
+                            enabled = !error.value
+                        )
+                    } else {
+                        Spacer(modifier = Modifier.weight(1.0f))
+
+                        com.finance.android.ui.components.TextButton(
+                            onClick = {
+                                outerNavController.navigate(Const.Routes.INPUT_PASSWORD)
+                            },
+                            text = "보내기",
+                            modifier = Modifier.withBottomButton(),
+                            buttonType = ButtonType.ROUNDED
+
+                        )
+                    }
                 }
             }
+        }
+
+
+        outerNavController.currentBackStackEntry
+            ?.savedStateHandle
+            ?.getLiveData<Boolean>("input_password")
+            ?.observeAsState()
+            ?.value?.let {
+                if (it) {
+                    AnimatedLoading(
+                        modifier = Modifier.background(androidx.compose.material3.MaterialTheme.colorScheme.surface),
+                        text = "송금하고 있어요"
+                    )
+
+                    LaunchedEffect(it) {
+                        if (!remitViewModel.requestRemit.value && Integer.parseInt(moneyValue) > 0) {
+                            remitViewModel.remitFromAccount(
+                                value = Integer.parseInt(moneyValue),
+                                receive = "",
+                                send = "",
+                                onSuccess = {
+                                    innerNavController.navigate("${Const.REMIT_OK_SCREEN}/$moneyValue/송금 완료")
+                                }
+                            )
+                        } else if (Integer.parseInt(moneyValue) > 0) {
+                            remitViewModel.remitFromPhone(
+                                value = Integer.parseInt(moneyValue),
+                                receive = "",
+                                send = "",
+                                onSuccess = {
+                                    innerNavController.navigate("${Const.REMIT_OK_SCREEN}/$moneyValue/$it")
+                                }
+                            )
+                        }
+                   }
+                }
         }
     }
 }
